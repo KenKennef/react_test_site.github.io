779d114371a1072160ec474c923c63822b9cb7e2
Kenneth Voss <kevo@bluefragments.com>
2021-01-28 12:47:59 +0100

    Navigation change

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import Services from './Components/services';
 import { AddIcCall, Assignment, HomeRounded, Info } from '@material-ui/icons';
 import Paralaxx from './Components/parallax';
 
-const drawerWidth = 220;
+const drawerWidth = 260;
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -29,29 +29,39 @@ const useStyles = makeStyles((theme: Theme) =>
 			display: 'flex',
 		},
 		appBar: {
-			transition: theme.transitions.create(['margin', 'width'], {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
+			// transition: theme.transitions.create(['margin', 'width'], {
+			// 	easing: theme.transitions.easing.sharp,
+			// 	duration: theme.transitions.duration.leavingScreen,
+			// }),
 		},
 		appBarShift: {
-			width: `calc(100% - ${drawerWidth}px)`,
-			marginLeft: drawerWidth,
-			transition: theme.transitions.create(['margin', 'width'], {
-				easing: theme.transitions.easing.easeOut,
-				duration: theme.transitions.duration.enteringScreen,
-			}),
+			// width: `calc(100% - ${drawerWidth}px)`,
+			// marginLeft: drawerWidth,
+			// transition: theme.transitions.create(['margin', 'width'], {
+			// 	easing: theme.transitions.easing.easeOut,
+			// 	duration: theme.transitions.duration.enteringScreen,
+			// }),
 		},
 		mainNavigation: {
 			margin: 0,
 			padding: 0,
-			display: 'flex',
-			flexFlow: 'colunm',
+			width: '100%',
+			justifyContent: 'flex-end',
+		},
+		desktopOnly: {
 			[theme.breakpoints.down('sm')]: {
 				display: 'none',
 			},
 			[theme.breakpoints.up('md')]: {
-				display: 'block',
+				display: 'flex',
+			},
+		},
+		mobileOnly: {
+			[theme.breakpoints.down('sm')]: {
+				display: 'flex',
+			},
+			[theme.breakpoints.up('md')]: {
+				display: 'none',
 			},
 		},
 		mainNavigationLink: {
@@ -62,12 +72,6 @@ const useStyles = makeStyles((theme: Theme) =>
 		},
 		menuButton: {
 			marginRight: theme.spacing(2),
-			// [theme.breakpoints.down('sm')]: {
-			// 	display: 'block',
-			// },
-			// [theme.breakpoints.up('md')]: {
-			// 	display: 'none',
-			// },
 		},
 		hide: {
 			display: 'none',
@@ -81,7 +85,7 @@ const useStyles = makeStyles((theme: Theme) =>
 			flexShrink: 0,
 		},
 		drawerPaper: {
-			width: drawerWidth,
+			width: '100%',
 		},
 		drawerHeader: {
 			display: 'flex',
@@ -89,7 +93,7 @@ const useStyles = makeStyles((theme: Theme) =>
 			padding: theme.spacing(0, 1),
 			// necessary for content to be below app bar
 			...theme.mixins.toolbar,
-			justifyContent: 'flex-end',
+			justifyContent: 'flex-start',
 		},
 		content: {
 			flexGrow: 1,
@@ -119,6 +123,12 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function PersistentDrawerLeft() {
 	const classes = useStyles();
 	const [open, setOpen] = React.useState(false);
+	const [active, setActive] = React.useState(0);
+
+	const handleMenuClick = (index: number) => {
+		setOpen(false);
+		setActive(index);
+	};
 
 	const handleDrawerOpen = () => {
 		setOpen(true);
@@ -127,6 +137,38 @@ export default function PersistentDrawerLeft() {
 	const handleDrawerClose = () => {
 		setOpen(false);
 	};
+
+	const renderNavigationItems = () =>
+		[
+			{ text: 'Eenim', link: '/', icon: 'Home' },
+			{ text: 'Ullamco', link: '/about', icon: 'Info' },
+			{ text: 'Daliquip', link: '/services', icon: 'Assignment' },
+			{ text: 'Aminim', link: '/contact', icon: 'AddIcCall' },
+		].map((item, index) => (
+			<Link to={item.link} className={classes.menuLink} key={index}>
+				<ListItem
+					button
+					onClick={() => handleMenuClick(index)}
+					selected={index === active}
+					style={
+						open
+							? {}
+							: {
+									color: '#ffffff',
+									textDecoration: index === active ? 'underline' : 'none',
+							  }
+					}
+				>
+					<ListItemIcon className={classes.mobileOnly}>
+						{item.icon === 'Home' ? <HomeRounded /> : null}
+						{item.icon === 'Info' ? <Info /> : null}
+						{item.icon === 'Assignment' ? <Assignment /> : null}
+						{item.icon === 'AddIcCall' ? <AddIcCall /> : null}
+					</ListItemIcon>
+					<h2>{item.text}</h2>
+				</ListItem>
+			</Link>
+		));
 
 	return (
 		<Router>
@@ -139,21 +181,28 @@ export default function PersistentDrawerLeft() {
 					})}
 					style={{ background: '#0E2F44' }}
 				>
-					<Toolbar>
+					<Toolbar style={{ border: '1px solid white' }}>
 						<IconButton
 							color='inherit'
 							aria-label='open drawer'
 							onClick={handleDrawerOpen}
 							edge='start'
-							className={clsx(classes.menuButton, open && classes.hide)}
+							className={clsx(
+								classes.menuButton,
+								open && classes.hide,
+								classes.mobileOnly
+							)}
 						>
 							<MenuIcon />
 						</IconButton>
-						<Link to='/' className={classes.headerLink}>
+						<Link to='/' className={clsx(classes.headerLink)}>
 							<Typography variant='h6' noWrap>
 								Duis aute irure dolor
 							</Typography>
 						</Link>
+						<List className={clsx(classes.mainNavigation, classes.desktopOnly)}>
+							{renderNavigationItems()}
+						</List>
 					</Toolbar>
 				</AppBar>
 				<Drawer
@@ -170,31 +219,7 @@ export default function PersistentDrawerLeft() {
 							<ChevronLeftIcon />
 						</IconButton>
 					</div>
-					<Divider />
-					<List>
-						{[
-							{ text: 'Eenim', link: '/', icon: 'Home' },
-							{ text: 'Ullamco', link: '/about', icon: 'Info' },
-							{ text: 'Daliquip', link: '/services', icon: 'Assignment' },
-							{ text: 'Aminim', link: '/contact', icon: 'AddIcCall' },
-						].map((item, index) => (
-							<Link to={item.link} className={classes.menuLink} key={index}>
-								<ListItem
-									button
-									onClick={() => handleDrawerClose()}
-									selected={window.location.pathname === item.link}
-								>
-									<ListItemIcon>
-										{item.icon === 'Home' ? <HomeRounded /> : null}
-										{item.icon === 'Info' ? <Info /> : null}
-										{item.icon === 'Assignment' ? <Assignment /> : null}
-										{item.icon === 'AddIcCall' ? <AddIcCall /> : null}
-									</ListItemIcon>
-									<h2>{item.text}</h2>
-								</ListItem>
-							</Link>
-						))}
-					</List>
+					<List>{renderNavigationItems()}</List>
 				</Drawer>
 				<main
 					className={clsx(classes.content, {
