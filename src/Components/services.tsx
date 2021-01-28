b2dbc9518c5b8d74a8572659b5675044b32aa34f
Kenneth Voss <kevo@bluefragments.com>
2021-01-28 10:57:26 +0100

    refactor

--- src/Components/services.tsx
+++ src/Components/services.tsx
@@ -10,15 +10,14 @@ export default function Services(props: any) {
 				<ServiceCard
 					image='https://static.s123-cdn-static.com/uploads/1980551/800_5700fe9d754d9.jpg'
 					heading='Duis aute irure dolor ffds'
-					text='Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+					text='At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga, ignissimos ducimus qui blanditiis praesentium voluptatum.'
 				/>
-				{/* <ServiceCard
+				<ServiceCard
 					image='https://static.s123-cdn-static.com/uploads/1980551/800_5c843748d80b3.jpg'
-					heading='Duis aute irure dolor'
-					text='Duis aute irure dolor'
-					buttonText='Lorum duis aute'
+					heading='Sed ut perspiciatis unde omnis iste natus'
+					text='Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat, aut rerum necessitatibus saepe eveniet.'
 					variant='image-right'
-				/> */}
+				/>
 			</Grid>
 		</Grid>
 	);
